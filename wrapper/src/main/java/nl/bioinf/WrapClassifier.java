29287a424562005aab3478b6dc31629df174a443
mvandermolen <m.van.der.molen@st.hanze.nl>
2023-10-20 15:55:41 +0200

    Updated Java wrapper

--- wrapper/src/main/java/nl/bioinf/WrapClassifier.java
+++ wrapper/src/main/java/nl/bioinf/WrapClassifier.java
@@ -1,19 +1,73 @@
 package nl.bioinf;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class WrapClassifier {
+    public static void main(String[] args){
+        WrapClassifier wrapper = new WrapClassifier();
+        wrapper.start();
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    private void start(){
+        //DELETE THIS TEST ONE PLEASE DONT FORGET
+        String test = "/homes/mvandermolen/thema_09/practicum_09/data/adjusted_penguin_data.arff";
+        System.out.println("Starting WrapClassifier...");
+        try{
+            Classifier model = loadClassifier();
+            Instances data = loadData(test);
+            System.out.println(data.toSummaryString());
+            Instances predictions = classifyData(model, data);
+            System.out.println(predictions);
+        } catch (Exception e){
+            throw new RuntimeException("Error occurred during runtime...");
+        }
+        // Create predictions
+    }
+
+    private Instances loadData(String filePath) throws Exception {
+        // Make sure the path given actually exists
+        if (! new File(filePath).exists()){
+            throw new IllegalArgumentException("Could not find the given file...");
+        }
+        ConverterUtils.DataSource reader = new ConverterUtils.DataSource(filePath);
+        Instances data = reader.getDataSet();
+        // Sets the class, what the program will predict. Typically, this is the last attribute.
+        data.setClassIndex(data.numAttributes() - 1);
+        return data;
+    }
+
+    private Classifier loadClassifier() throws IOException {
+        // Get Logistic Model from the resources file
+        URL url = getClass().getResource("/Logistic_Model.model");
+        try {
+            // Double check that it isn't null
+            assert url != null;
+            // Return a classifier with the resource file
+            return (Classifier) SerializationHelper.read(url.getFile());
+        } catch (Exception e) {
+            throw new IOException("Could not read from file..." + e.getMessage());
         }
     }
+
+    private Instances classifyData(Classifier model, Instances data) {
+        Instances labeled = new Instances(data);
+        for (int index = 0; index < data.numInstances(); index++) {
+            double label = 0;
+            try {
+                label = model.classifyInstance(data.instance(index));
+            } catch (Exception e) {
+                throw new RuntimeException("Something went wrong while classifying.." + e.getMessage());
+            }
+            labeled.instance(index).setClassValue(label);
+        }
+        return labeled;
+    }
+
+
 }
